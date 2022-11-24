7cf9faf3c5c453cac0e8db178a3a7a4ef08847eb
samky444 <samuel.kihuha@gmail.com>
2022-11-23 19:56:12 -0800

    Update week4.js
    I have commented on the last portion

--- week4.js
+++ week4.js
@@ -3,18 +3,18 @@
 //alert(pi.toFixed(2)); // method of the number object
 
 // string
-// const myName = "Samuel Kiarie";
-// const myNameUpper = myName.toUpperCase();
-// alert(myNameUpper);
+const myName = "Samuel Kiarie";
+const myNameUpper = myName.toUpperCase();
+alert(myNameUpper);
 
 // Instatiation of an object
-let school = {
-name: 'Pierce College',
-location: 'Pierce County',
-established: '1956',
-displayInfo : function(){
-alert(`${school.name} was established in ${school.established} at ${school.location}`);
+// let school = {
+// name: 'Pierce College',
+// location: 'Pierce County',
+// established: '1956',
+// displayInfo : function(){
+// alert(`${school.name} was established in ${school.established} at ${school.location}`);
 
-    }
-}
-school.displayInfo();
+//     }
+// }
+// school.displayInfo();
