af7cac884ddd9dd4addfcbc61499e94dab1ad92d
samky444 <samuel.kihuha@gmail.com>
2022-11-23 19:28:48 -0800

    Create week4.js
    initial commit

---
+++ week4.js
@@ -0,0 +1,20 @@
+// Number object
+//const pi = 3.14159;
+//alert(pi.toFixed(2)); // method of the number object
+
+// string
+// const myName = "Samuel Kiarie";
+// const myNameUpper = myName.toUpperCase();
+// alert(myNameUpper);
+
+// Instatiation of an object
+let school = {
+name: 'Pierce College',
+location: 'Pierce County',
+established: '1956',
+displayInfo : function(){
+alert(`${school.name} was established in ${school.established} at ${school.location}`);
+
+    }
+}
+school.displayInfo();
